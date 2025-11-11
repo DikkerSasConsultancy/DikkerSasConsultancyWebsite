6a72a3fcdbc9b31b6d1d16f13e01545a52d69025
DikkerSasConsultancy <susansas@dikkersasconsultancy.nl>
2025-11-11 16:27:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const tabs = document.querySelectorAll('.tab');
+const contents = document.querySelectorAll('.tab-content');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active'));
+        tab.classList.add('active');
+
+        const target = tab.dataset.tab;
+        contents.forEach(c => {
+            c.classList.remove('active');
+            if(c.id === target) c.classList.add('active');
+        });
+    });
+});
